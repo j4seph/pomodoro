ce30d21aea06a3ca04c5a7137519702ef1805fc3
j4seph <j4seph@gmail.com>
2023-02-13 11:10:23 +0000

    Updating MIME types

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+module.exports = {
+  serve: {
+    mimeTypes: {
+      'application/javascript': ['js', 'jsx']
+    }
+  }
+}
